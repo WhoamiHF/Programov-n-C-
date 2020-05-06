fed3818f4b8c6f4ba413ae453fdeaf11f978cd14
WhoamiHF <45389520+WhoamiHF@users.noreply.github.com>
2020-05-06 08:59:10 +0200

    Add files via upload

--- stencil1d.hpp
+++ stencil1d.hpp
@@ -10,16 +10,27 @@
 template<typename ET>
 class circle;
 
-template<typename ET, typename SF>
-class forwarder {
+
+template<typename ET>
+class package 
+{
 public:
-	
-	void operator()(void* context, typename std::vector<ET>::iterator beg, typename std::vector<ET>::iterator end, size_t G, SF& sf, size_t g, size_t index, size_t threads)
-	{
-		static_cast<circle<ET>*>(context)->oneThreadJob(beg, end, G, sf, g, index, threads);
-	}
+	std::mutex mtx;
+	std::condition_variable condition;
+	std::queue<std::vector<ET>> channel;
 };
 
+template<typename ET, typename SF>
+class forwarder 
+{
+public:
+	void operator()(void* context, typename std::vector<ET>::iterator beg, typename std::vector<ET>::iterator end, size_t G, SF& sf, size_t g, size_t index,
+		size_t threads, typename std::vector<ET>::iterator res
+		,package<ET>& toLeft, package<ET>& toRight, package<ET>& fromLeft, package<ET>& fromRight)
+	{
+		static_cast<circle<ET>*>(context)->oneThreadJob(beg, end, G, sf, g, index, threads,res,toLeft,toRight,fromLeft,fromRight);
+	}
+};
 
 template<typename ET>
 class circle
@@ -27,27 +38,27 @@ class circle
 public:
 	circle(std::size_t s) :field(std::vector<ET>(s)) {}
 
-	std::size_t size() const 
+	std::size_t size() const
 	{
 		return field.size();
 	}
 	//Computes real index by adding size() to parametr and then aplying mod size(),sets value of índexed element
-	void set(std::ptrdiff_t x, const ET& v) 
+	void set(std::ptrdiff_t x, const ET& v)
 	{
 		std::ptrdiff_t index = (x + size()) % size();
 		field[index] = v;
 	}
 	//Computes real index by adding size() to parametr and then aplying mod size(),gets value of índexed element
-	decltype(auto) get(std::ptrdiff_t x) const 
+	decltype(auto) get(std::ptrdiff_t x) const
 	{
 		std::ptrdiff_t index = (x + size()) % size();
 		return field[index];
 	}
 	//this is the function which does each thread. First it sends messages to its neighbors, then recieves from them and then does G (or less) generations. 
 	template<typename SF>
-	void oneThreadJob(typename std::vector<ET>::iterator beg, typename std::vector<ET>::iterator end, size_t G, SF& sf, size_t g, size_t index, size_t threads)
+	void oneThreadJob(typename std::vector<ET>::iterator beg, typename std::vector<ET>::iterator end, size_t G, SF& sf, size_t g, size_t index, size_t threads,
+		typename std::vector<ET>::iterator res,package<ET>& toLeft, package<ET>& toRight, package<ET>& fromLeft, package<ET>& fromRight)
 	{
-
 		threeVectors storage;
 		storage.thisPart = std::vector(beg, end);
 
@@ -57,46 +68,42 @@ public:
 		secondaryStorage.leftG = std::vector<ET>(G);
 		secondaryStorage.thisPart = std::vector<ET>(storage.thisPart.size());
 		secondaryStorage.rightG = std::vector<ET>(G);
-		for(size_t generationIndex = 1;generationIndex<=g;generationIndex+=G)
+		for (size_t generationIndex = 1; generationIndex <= g; generationIndex += G)
 		{
 
 			//sending
 			{
-				std::lock_guard<std::mutex> lock(mutexesToLeft[index]);
-				channelsToLeft[index].push(std::vector<ET>(storage.thisPart.begin(), storage.thisPart.begin() + G));
+				std::lock_guard<std::mutex> lock(toLeft.mtx);
+				toLeft.channel.push(std::vector<ET>(storage.thisPart.begin(), storage.thisPart.begin() + G));
 			}
-			cond_variables_to_left[index].notify_one();
+			toLeft.condition.notify_one();
 			// uses index of thread, for each two neighbor threads there are two channels - toLeft and toRight
 			{
-				std::lock_guard<std::mutex> lock(mutexesToRight[index]);
-				channelsToRight[index].push(std::vector<ET>(storage.thisPart.end() - G, storage.thisPart.end()));
+				std::lock_guard<std::mutex> lock(toRight.mtx);
+				toRight.channel.push(std::vector<ET>(storage.thisPart.end() - G, storage.thisPart.end()));
 			}
-			cond_variables_to_right[index].notify_one();
-			//recieving   @todo:notifications - not needed 
-			//@todo make sure index are correct
-
+			toRight.condition.notify_one();
+			//recieving
+			//@todo make sure indexes are correct
 			{
-				size_t indexRight = (index + 1) % threads;
-				std::unique_lock<std::mutex> lock(mutexesToLeft[indexRight]);
-				while (channelsToLeft[indexRight].empty())
+				std::unique_lock<std::mutex> lock(fromRight.mtx);
+				while (fromRight.channel.empty())
 				{
-					cond_variables_to_left[indexRight].wait(lock);
+					fromRight.condition.wait(lock);
 				}
-					storage.rightG = channelsToLeft[indexRight].front();
-					channelsToLeft[indexRight].pop();
+				storage.rightG = fromRight.channel.front();
+				fromRight.channel.pop();
 			}
 			{
-				size_t indexLeft = (index - 1 + threads) % threads;
-				std::unique_lock<std::mutex> lock2(mutexesToRight[indexLeft]);
-				while (channelsToRight[indexLeft].empty())
+				std::unique_lock<std::mutex> lock(fromLeft.mtx);
+				while (fromLeft.channel.empty())
 				{
-					cond_variables_to_right[indexLeft].wait(lock2);
+					fromLeft.condition.wait(lock);
 				}
-					storage.leftG = channelsToRight[indexLeft].front();
-					channelsToRight[indexLeft].pop();
+				storage.leftG = fromLeft.channel.front();
+				fromLeft.channel.pop();
 			}
-
-			//swap pointers
+			//@todo:swap pointers
 			//computing G (or less) generations. In each generation it computes only valid part of the vector. 
 			for (size_t i = 1; i <= G && i <= g; i++)
 			{
@@ -107,59 +114,63 @@ public:
 				std::swap(secondaryStorage, storage);
 			}
 		}
-		//move
-		//@todo write back -> warning write with caution
+		//write to result, which then will be moved to original vector
 		{
 			std::unique_lock<std::mutex> lock(mutexToWriteData);
 			for (size_t i = 0; i < storage.thisPart.size(); i++)
 			{
-				*(beg + i) = storage[i + storage.leftG.size()];
+				*(res + i) = storage[i + storage.leftG.size()];
 			}
 		}
 		//@todo clean (/ local in run) make sure not locked mutex
 		//one thread,low amount of generations
-
 	}
 	//run divides the field to threads
 	template<typename SF>
-	void run(SF&& sf, std::size_t g, std::size_t thrs = std::thread::hardware_concurrency()) 
+	void run(SF&& sf, std::size_t g, std::size_t thrs = std::thread::hardware_concurrency())
 	{
 		std::vector<std::thread> workers;
 		size_t W = size() / thrs;
-		size_t G = W/8;
+		size_t G = W / 32;
 		if (G < 1) { G += 1; }
-		mutexesToLeft = std::vector<std::mutex>(thrs);
-		mutexesToRight = std::vector<std::mutex>(thrs);
-		cond_variables_to_left = std::vector<std::condition_variable>(thrs);
-		cond_variables_to_right = std::vector<std::condition_variable>(thrs);
-		channelsToLeft = std::vector<std::queue<std::vector<ET>>>(thrs);
-		channelsToRight = std::vector<std::queue<std::vector<ET>>>(thrs);
-
+		std::vector<package<ET>> toLeft(thrs);
+		std::vector<package<ET>> toRight(thrs);
+		std::vector<ET> result(size());
+		typename std::vector<ET>::iterator startOfResult = result.begin();
 		typename std::vector<ET>::iterator startingPosition = field.begin();
-		for (size_t th = 0; th < thrs; th++) 
+		for (size_t th = 0; th < thrs; th++)
 		{
-			if (size() % thrs > th) 
+			if (size() % thrs > th)
 			{
-				workers.push_back(std::thread(forwarder<ET,SF>(),this,startingPosition, startingPosition + W + 1, G, std::ref(sf), g, th, thrs));
+				workers.push_back(std::thread(forwarder<ET, SF>(), this, startingPosition, startingPosition + W + 1, G, std::ref(sf), g, th, thrs, startOfResult
+					,std::ref(toLeft[th]), std::ref(toRight[th]), std::ref(toRight[(th - 1 + thrs) % thrs]), std::ref(toLeft[(th + 1) % thrs])));
+
+
 				startingPosition += (W + 1);
+				startOfResult += (W + 1);
 			}
 			else
 			{
-				workers.push_back(std::thread(forwarder<ET, SF>(), this, startingPosition, startingPosition + W, G, std::ref(sf), g, th, thrs));
+				workers.push_back(std::thread(forwarder<ET, SF>(), this, startingPosition, startingPosition + W, G, std::ref(sf), g, th, thrs, startOfResult
+					, std::ref(toLeft[th]), std::ref(toRight[th]), std::ref(toRight[(th - 1 + thrs) % thrs]), std::ref(toLeft[(th + 1) % thrs])));
 				startingPosition += W;
+				startOfResult += W;
 			}
 		}
 		for (auto& t : workers)
+		{
 			t.join();
+		}
+		field = std::move(result);
 	}
 	//each thread creates two of this object. This class contains three parts - leftG will be recieved from left, thisPart is computedby this thread
 	//and rightG will be recieved from right neighbor. It can be indexed without knowing how the parts are divided.
-	class threeVectors 
+	class threeVectors
 	{
 	public:
-		decltype(auto) operator[](size_t index) 
+		decltype(auto) operator[](size_t index)
 		{
-			if (index < leftG.size()) 
+			if (index < leftG.size())
 			{
 				return leftG[index];
 			}
@@ -167,13 +178,13 @@ public:
 			{
 				return thisPart[index - leftG.size()];
 			}
-			else 
+			else
 			{
 				return rightG[index - leftG.size() - thisPart.size()];
 			}
 
 		}
-		size_t size() 
+		size_t size()
 		{
 			return leftG.size() + thisPart.size() + rightG.size();
 		}
@@ -181,15 +192,9 @@ public:
 		std::vector<ET> thisPart;
 		std::vector<ET> rightG;
 	};
-	
+
 private:
 	std::vector<ET> field;
-	std::vector<std::mutex> mutexesToRight;
-	std::vector<std::mutex> mutexesToLeft;
-	std::vector<std::condition_variable> cond_variables_to_right;
-	std::vector<std::condition_variable> cond_variables_to_left;
-	std::vector<std::queue<std::vector<ET>>> channelsToRight;
-	std::vector<std::queue<std::vector<ET>>> channelsToLeft;
 	std::mutex mutexToWriteData;
 };
 
